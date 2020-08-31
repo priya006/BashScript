71d18741b4b3fd21caf3af7e449e6f664ec80d92
pboopathi <pboopathi@ebay.com>
2020-08-31 14:51:13 -0700

    color variables added

---
+++ git_update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# alias gitupdate='(for l in `find . -name .git | xargs -i dirname {}` ; do cd $l; pwd; git pull; echo "pulled";  cd -; done)'
+
+NOERROR='\033[1;92m'
+NOCOLOR='\033[0m' # No Color
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo -e ${NOERROR}Up-to-date
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+else
+    echo "Diverged"
+fi
+
