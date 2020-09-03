54b65fdd55f7fe8d50435b01de585ba19fbb9df1
pboopathi <pboopathi@ebay.com>
2020-09-02 22:38:13 -0700

    Added code to grep for a word in current dorectory

--- recursive_grep_search.sh
+++ recursive_grep_search.sh
@@ -7,9 +7,9 @@ currentpath=`pwd`
 echo "The current path is $currentpath"
 # searchword=bin
 # echo "Enter the word to search: $1"
-grep -r -n -c  "$1" $currentpath
+grep -r -c  "$1" .
 }
-findpath 
+findpath $1
 
 
 
